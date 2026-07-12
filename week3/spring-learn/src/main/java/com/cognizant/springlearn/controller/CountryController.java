b5dd29938d9bdd404f02f85e17666155a9f4c7b9
ismailKachanchery <ismailkachanchery@gmail.com>
2026-07-12 22:52:36 +0530

    Week3 Module2 Sub3 - Country REST API

---
+++ week3/spring-learn/src/main/java/com/cognizant/springlearn/controller/CountryController.java
@@ -0,0 +1,23 @@
+package com.cognizant.springlearn.controller;
+
+import com.cognizant.springlearn.Country;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CountryController {
+
+    @GetMapping("/country")
+    public Country getCountry() {
+
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("country.xml");
+
+        Country country = context.getBean("country", Country.class);
+
+        context.close();
+
+        return country;
+    }
+}
