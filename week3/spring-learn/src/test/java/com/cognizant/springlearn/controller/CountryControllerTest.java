546a406b6cb075300f92ccb7dc39c12a8e7677c4
ismailKachanchery <ismailkachanchery@gmail.com>
2026-07-22 11:50:05 +0530

    Week3 Module2 Sub6 - MockMvc testing

---
+++ week3/spring-learn/src/test/java/com/cognizant/springlearn/controller/CountryControllerTest.java
@@ -0,0 +1,27 @@
+package com.cognizant.springlearn.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class CountryControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testGetCountry() throws Exception {
+
+        mockMvc.perform(get("/country"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").value("IN"))
+                .andExpect(jsonPath("$.name").value("India"));
+    }
+}
