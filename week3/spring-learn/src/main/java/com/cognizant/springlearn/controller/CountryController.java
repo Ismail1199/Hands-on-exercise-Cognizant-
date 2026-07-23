a98fbf5b545442bdfd55d83bd8851c1b39ae4909
ismailKachanchery <ismailkachanchery@gmail.com>
2026-07-23 14:28:29 +0530

    Week3 Module4 Sub6 - Log request headers

--- week3/spring-learn/src/main/java/com/cognizant/springlearn/controller/CountryController.java
+++ week3/spring-learn/src/main/java/com/cognizant/springlearn/controller/CountryController.java
@@ -8,6 +8,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import jakarta.servlet.http.HttpServletRequest;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,11 +24,22 @@ public class CountryController {
         this.countryService = countryService;
     }
 
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(CountryController.class);
+
     @GetMapping("/countries/{code}")
-    public Country getCountry(@PathVariable String code)
+    public Country getCountry(@PathVariable String code,
+                              HttpServletRequest request)
             throws CountryNotFoundException {
 
-        return countryService.getCountry(code);
+        LOGGER.info("START");
+        LOGGER.info("Accept Header : {}", request.getHeader("Accept"));
+
+        Country country = countryService.getCountry(code);
+
+        LOGGER.info("END");
+
+        return country;
     }
 
     @GetMapping("/country")
